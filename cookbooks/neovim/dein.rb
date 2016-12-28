06e2377b0b8eba905448ef3d2aa6826644785e75
Shimpei Makimoto <makimoto@tsuyabu.in>
2016-12-29 02:09:08 +0900

    Revert "d to f"
    This reverts commit 4924ce81e40854ce35fa6f0823b5eb284659abd3.

--- cookbooks/neovim/dein.rb
+++ cookbooks/neovim/dein.rb
@@ -1,5 +1,5 @@
 execute "mkdir -p #{ENV['HOME']}/.config/nvim/dein"
 execute "Install dein" do
   command "curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh -o /tmp/dein_installer.sh && sh -x /tmp/dein_installer.sh #{ENV['HOME']}/.config/nvim/dein"
-  not_if "test -f ~/.config/nvim/dein/repos/github.com/Shougo/dein.vim"
+  not_if "test -d ~/.config/nvim/dein/repos/github.com/Shougo/dein.vim"
 end
