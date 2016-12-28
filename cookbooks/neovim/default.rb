f65e74bcf9fb4f510025f2db1273589d86ff04d6
Shimpei Makimoto <makimoto@tsuyabu.in>
2016-12-29 02:10:48 +0900

    Use directory resource

--- cookbooks/neovim/default.rb
+++ cookbooks/neovim/default.rb
@@ -4,7 +4,7 @@ end
 
 package 'neovim'
 
-execute "mkdir -p #{ENV['HOME']}/.config/nvim"
+directory "#{ENV['HOME']}/.config/nvim"
 remote_file "#{ENV['HOME']}/.config/nvim/init.vim" do
   source "files/nvim/init.vim"
 end
--- cookbooks/neovim/dein.rb
+++ cookbooks/neovim/dein.rb
@@ -1,5 +1,7 @@
-execute "mkdir -p #{ENV['HOME']}/.config/nvim/dein"
+dir = "#{ENV['HOME']}/.config/nvim/dein"
+
+directory dir
 execute "Install dein" do
-  command "curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh -o /tmp/dein_installer.sh && sh -x /tmp/dein_installer.sh #{ENV['HOME']}/.config/nvim/dein"
+  command "curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh -o /tmp/dein_installer.sh && sh -x /tmp/dein_installer.sh #{dir}"
   not_if "test -d ~/.config/nvim/dein/repos/github.com/Shougo/dein.vim"
 end
