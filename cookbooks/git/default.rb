92a4c54a39c32fbe602d63e570ec3988054f20de
Shimpei Makimoto <makimoto@tsuyabu.in>
2016-12-28 20:06:39 +0900

    Add gitignore

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -1,5 +1,9 @@
 package 'git'
   
 remote_file "#{ENV['HOME']}/.gitconfig" do
-  source 'files/.gitconfig'
+  source 'files/gitconfig'
+end
+
+remote_file "#{ENV['HOME']}/.gitignore" do
+  source 'files/gitignore'
 end
