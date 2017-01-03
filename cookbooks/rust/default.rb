cbdea100a7f05b3de50c2ac28b762e585ef43cd8
Shimpei Makimoto <makimoto@tsuyabu.in>
2017-01-03 20:43:12 +0900

    rust: Fix a bug to detect binary existing

--- cookbooks/rust/default.rb
+++ cookbooks/rust/default.rb
@@ -1,9 +1,9 @@
 package 'rust'
 
 execute "cargo install rustfmt" do
-  not_if "test -e `which rustfmt`"
+  not_if 'test -e "$(which rustfmt)"'
 end
 
 execute "cargo install racer" do
-  not_if "test -e `which racer`"
+  not_if 'test -e "$(which racer)"'
 end
